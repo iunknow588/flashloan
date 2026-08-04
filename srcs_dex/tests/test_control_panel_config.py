67c031897c922bdc197389691f2d9edd45a90579
CDao <139673091+iunknow588@users.noreply.github.com>
2026-08-04 13:34:43 +0800

    Remove redundant root srcs_dex copy

--- srcs_dex/tests/test_control_panel_config.py
+++
@@ -1,9 +0,0 @@
-from web.control_panel_config import sanitize_strategy_config, unified_sampling_profile
-
-
-def test_sampling_profile_allows_configured_200ms_window():
-    config = sanitize_strategy_config({"BINANCE_CHANGE_WINDOW_SECONDS": 0.2})
-    profile = unified_sampling_profile(config)
-
-    assert config["BINANCE_CHANGE_WINDOW_SECONDS"] == 0.2
-    assert profile["seconds"] == 0.2
